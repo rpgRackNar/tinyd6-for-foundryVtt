58b008abfdef02f85ab404f533fbf3312a724ef3
Jan Drenkhahn <jan-dennisd@outlook.at>
2023-01-28 20:18:50 +0100

    add storybook

---
+++ .storybook/main.cjs
@@ -0,0 +1,9 @@
+// .storybook/main.js|ts
+
+module.exports = {
+  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  core: {
+    builder: '@storybook/builder-vite', // 👈 The builder enabled here.
+  },
+};
